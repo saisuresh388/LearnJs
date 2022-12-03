de9aeabe96447b7eb6c5865edb08e62b32047273
Saisuresh Developer <saisuresh388@gmail.com>
2022-12-03 12:46:21 +0530

    code optimized for even number

--- js/arrayTopics.js
+++ js/arrayTopics.js
@@ -1,10 +1,14 @@
-var arrayOne = [1,2,3,4,5];
+var arrayOne = [1, 2, 3, 4, 5];
 // find a even number
-function fun(array){
-    var arrayResult = [];
-    for ( let i=0; i < array.length ; i++ ){
-        array[i]%2 == 0 && arrayResult.push(array[i]);
-    }
-    console.log(`The even array is = ${ arrayResult }`);
+function fun(array) {
+  var arrayResult = [];
+  for (let i = 0; i < array.length; i++) {
+    array[i] % 2 == 0 && arrayResult.push(array[i]);
+  }
+  console.log(`The even array is = ${arrayResult}`);
 }
 fun(arrayOne);
+
+// Alternate way
+let resutArray=arrayOne.filter(val=>val% 2 == 0)
+console.log(resutArray) // output:[2,4]
